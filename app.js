1fa32645297e02abdc04ec39a840d7e9b6931454
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2021-02-18 18:27:46 +0100

    Done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+// Here I Take the reference at the chooser of game Type, a choice between Computer or Player
+
 let playersOrComputer = Array.prototype.slice.call(document.querySelectorAll('input[name=player-choice-computer-person]'));
 
 playersOrComputer.forEach(choice => {
     choice.addEventListener('click', decidePlayerOrComputer, false)
 })
 
+// Here i Store the Value of the choice, which would be Computer or Player
+
 function decidePlayerOrComputer() {
     choiceComputerPlayer = this.value;
-    this.checked = false;
-
-    console.log(choiceComputerPlayer)
 }
 
 let choiceComputerPlayer;
@@ -17,15 +18,16 @@ const msg = document.querySelector('.message')
 let mark;
 let cells;
 
-const letTheGameBegins = () => {
-    initialName()
-}
+// I give the value of the choosen Sign to mark
+// After that, the gridBuilder function will start.
 
 function setPlayer() {
     mark = this.value;
-    this.checked = false;
+    // this.checked = false;
     gridBuilder();
 }
+
+// This function decides how the game is set, against the computer or a Player
 
 function playerMoves() {
     if(choiceComputerPlayer == 'Computer'){
@@ -42,9 +44,9 @@ function playerMoves() {
             switchPlayer();
         }
     }
-
-    
 }
+
+// Here I switch between the marks, after every moves
 
 const switchPlayer = () => {
     if(mark == 'X'){
@@ -53,6 +55,10 @@ const switchPlayer = () => {
         mark = 'X'
     }
 }
+
+// This Function is for the computer moves
+// where i made an empty array for the empty cells
+// For every empty cell, i push it inside the array
 
 const computerMove = () => {
     let emptyCells = [];
@@ -64,11 +70,15 @@ const computerMove = () => {
         }
     })
 
+    // Now the computer will randomly put his sign for every empty cells inside the array
+
     random = Math.ceil(Math.random() * emptyCells.length) - 1;
     emptyCells[random].textContent = mark;
     checkRows();
     switchPlayer();
 }
+
+// This is the win's condition, where I check if the three cells are good for a win
 
 const checkRowsForWinner = (tile1, tile2, tile3) => {
     if(tile1.textContent == mark && tile2.textContent == mark && tile3.textContent == mark){
@@ -98,7 +108,11 @@ const resetGame = () => {
     })
 
     grid.innerHTML = '';
+    gridBuilder();
 }
+
+// Here I'm creating the grid, where the value can't surpass nine cells
+// the id will be equal c + i(index) of the for loop
 
 const gridBuilder = () => {
     for(let i = 1; i <= 9; i++){
@@ -111,6 +125,9 @@ const gridBuilder = () => {
     }
 }
 
+// This is made for the initial choice of the sign
+// looking for the choosen option, between X or O
+
 let players = Array.prototype.slice.call(document.querySelectorAll('input[name=player-choice]'));
 
 players.forEach(choice => {
@@ -118,6 +135,8 @@ players.forEach(choice => {
 })
 
 let resetButton = document.querySelector('button');
+
+// This is the reset button, when I click on it, I prevent the reload of the page
 
 resetButton.addEventListener('click', e => {
     e.preventDefault();
